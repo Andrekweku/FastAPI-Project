6f59b52cbcff4373195f0a1d4c41133a6f0f8674
andrekweku <32077921+Andrekweku@users.noreply.github.com>
2023-01-16 21:48:05 +0000

    Update conftest.py

--- Test/conftest.py
+++ Test/conftest.py
@@ -11,25 +11,13 @@ from app.Oauth2 import create_access_token
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password123@localhost:5432/Fastapi_test"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Test_Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base.metadata.create_all(bind=engine)
 
-
-# def override_get_db():
-#     db = Test_Sessionlocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
-
-# client = TestClient(app)
 
 @pytest.fixture
 def session():
@@ -49,13 +37,9 @@ def client(session):
         finally:
             session.close()
     app.dependency_overrides[get_db] = override_get_db
-    #clear our database for the code runs
-    # Base.metadata.drop_all(bind=engine)
-    #run our code before we run our test
-    # Base.metadata.create_all(bind=engine)
+    
     yield  TestClient(app)
-    # Base.metadata.drop_all(bind=engine) - you can use this too and delete the one above
-    #run our code after our test finishes
+
 
 @pytest.fixture
 def test_user2(client):
