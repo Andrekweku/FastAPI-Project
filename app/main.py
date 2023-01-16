68f16130a669f00fe2722200e4763ccd6b9e7fab
andrekweku <32077921+Andrekweku@users.noreply.github.com>
2023-01-16 21:41:57 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -30,35 +30,3 @@ app.include_router(vote.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-# while True:
-
-#     try:
-#         conn = psycopg2.connect(
-#             host="localhost",
-#             database="19hrs - Tutorial",
-#             user="postgres",
-#             password="GOOGLE4LYF",
-#             cursor_factory=RealDictCursor,
-#         )
-#         cursor = conn.cursor()
-#         print("Database connection was successful.")
-#         break
-#     except Exception as error:
-#         print("Connecting to Database failed")
-#         print("Error: ", error)
-#         time.sleep(2)
-
-# my_posts = [
-#     {"title": "title of post 1", "content": "content of post 1", "id": 1},
-#     {"title": "favorite foods", "content": "I love Pizza", "id": 2},
-# ]
-
-
-# @app.get("/sqlalchemy")
-# def test_post(db: Session = Depends(get_db)):
-
-#     posts = db.query(models.Post).all()
-#     # print(posts)
-#     return {"data": posts}
